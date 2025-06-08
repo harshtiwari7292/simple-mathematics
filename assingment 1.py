e16912a90f7e96506a18e36f3d3050dc922456d4
harshtiwari7292 <126195252+harshtiwari7292@users.noreply.github.com>
2025-06-08 15:07:56 +0530

    Add files via upload

---
+++ assingment 1.py
@@ -0,0 +1,14 @@
+#TASK 1=perform basic mathemetical expresion
+
+a=int(input("enter your first number:"))
+b=int(input("enter your second number:"))
+print("Addition :",a+b)
+print("Substraction :", a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+
+#TASK 2=creat a personlize greeting
+a=input("enter your first name:")
+b=input("enter your last name:")
+
+print("hello",a,b,"! welcome to the python program")
